b45333db8bf490bf28234ff5f53bbdd2c97d3c5a
kgandhi09 <kushal009gandhi@gmail.com>
2021-01-27 16:22:39 +0530

    server

--- Db/connection.db.js
+++ Db/connection.db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
-const Grid = require('gridfs-stream')
 
 const MongoURI = "mongodb+srv://dbuser:dbuser@cluster0.8dogy.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const connectDb = async () => {
   await mongoose.connect(MongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true, 
-    useFindAndModify: false, 
-    useCreateIndex: true
+    // useFindAndModify: false, 
+    // useCreateIndex: true
   })
   .then(() => console.log("db connected..!"))
   .catch(err => console.log("no connection..!", err));
