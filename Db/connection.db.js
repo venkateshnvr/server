e43c92071deda4c37aae1d70b4504f18668f8d69
kgandhi09 <kushal009gandhi@gmail.com>
2021-01-27 16:09:14 +0530

    database details

--- Db/connection.db.js
+++ Db/connection.db.js
@@ -5,7 +5,9 @@ const MongoURI = "mongodb+srv://dbuser:dbuser@cluster0.8dogy.mongodb.net/<dbname
 const connectDb = async () => {
   await mongoose.connect(MongoURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true, 
+    useFindAndModify: false, 
+    useCreateIndex: true
   })
   .then(() => console.log("db connected..!"))
   .catch(err => console.log("no connection..!", err));
