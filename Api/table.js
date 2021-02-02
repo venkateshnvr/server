960511c41a02ae48cb5794bdfe2e2fae1760b15b
venkatesh_nvr <venkatesh.nvrm@gmail.com>
2021-02-02 14:56:48 +0530

    tables

--- Api/table.js
+++ Api/table.js
@@ -3,9 +3,11 @@ const robotTabelModel = require("../Db/model.db.table");
 const route = express.Router();
 
 route.post("/tabel", (req, res) => {
-  let date = new Date();
+  let date = new Date(req.body.date);
+  console.log('date', date)
   let setdate =
-    date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  console.log('setdate', setdate)
   let roboTabelObject = {};
   roboTabelObject.date = setdate;
   roboTabelObject.table = req.body.tabel.map(data => {
@@ -22,7 +24,14 @@ route.post("/tabel", (req, res) => {
 });
 
 route.get("/tabel/:date", (req, res) => {
+  console.log(req.params.date)
   robotTabelModel.find({ date: req.params.date }).then(data => {
+    res.json(data);
+  });
+});
+
+route.get("/tabel", (req, res) => {
+  robotTabelModel.find().then(data => {
     res.json(data);
   });
 });
